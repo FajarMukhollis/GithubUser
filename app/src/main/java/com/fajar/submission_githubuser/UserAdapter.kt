1fe459fac91c1d6a86d6197f3f1287b513e5628e
Fajar Mukhollis <fmukhollis@gmail.com>
2022-03-07 14:58:18 +0700

    Initial commit

---
+++ app/src/main/java/com/fajar/submission_githubuser/UserAdapter.kt
@@ -0,0 +1,57 @@
+package com.fajar.submission_githubuser
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.fajar.submission_githubuser.DetailActivity.Companion.EXTRA_USER
+import com.fajar.submission_githubuser.databinding.ItemListUserBinding
+import com.fajar.submission_githubuser.model.User
+
+class UserAdapter: RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+
+    private val listUser = ArrayList<User>()
+
+    fun setAllData(data: List<User>) {
+        listUser.apply {
+            clear()
+            addAll(data)
+        }
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val view = ItemListUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return UserViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.bind(listUser[position])
+    }
+
+    override fun getItemCount(): Int = listUser.size
+
+    inner class UserViewHolder(private val view: ItemListUserBinding): RecyclerView.ViewHolder(view.root) {
+
+        fun bind(user: User) {
+
+            view.apply {
+                tvUsername.text = user.username
+                tvName.text = user.name
+            }
+
+            Glide.with(itemView.context)
+                .load(user.avatar)
+                .apply(RequestOptions.circleCropTransform())
+                .into(view.ivImgUser)
+
+            itemView.setOnClickListener {
+                val intent = Intent(itemView.context, DetailActivity::class.java)
+                intent.putExtra(EXTRA_USER, user)
+                itemView.context.startActivity(intent)
+            }
+        }
+    }
+}
